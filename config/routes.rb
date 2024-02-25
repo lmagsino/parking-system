371b2788699ceb47d9bc00a4e08002df332dd8f5
Leo Magsino <leo.magsino819@gmail.com>
2024-02-25 14:32:44 +0800

    added parking_slot, parking_transaction and park to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :parking_lots, :except => [:new, :edit]
-  resources :parking_slots, :except => [:new, :edit]
+  resources :parking_slots, :only => [:new, :create, :index]
+  resources :parking_transactions, :only => [:index]
 
   get "up" => "rails/health#show", as: :rails_health_check
+  post 'vehicles/park', :to => 'vehicles#park'
 end
