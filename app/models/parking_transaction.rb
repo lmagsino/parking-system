86b114dda2947e4256385da265a2ed02ad0e3ed1
Leo Magsino <leo.magsino819@gmail.com>
2024-02-25 09:31:57 +0800

    added parking transaction model

---
+++ app/models/parking_transaction.rb
@@ -0,0 +1,34 @@
+class ParkingTransaction
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  belongs_to :parking_slot
+  belongs_to :vehicle
+
+
+
+  state_machine :status, :initial => :pending do
+    event :start do
+      transition :pending => :started
+    end
+
+    event :complete do
+      transition :started => :completed
+    end
+
+    before_transition :on => :complete, :do => :compute_amount
+    after_transition :on => :start, :do => :occupy_parking_slot
+    after_transition :on => :complete, :do => :release_parking_slot
+  end
+
+
+
+  scope :completed, -> do
+    where :status => :completed
+  end
+
+  scope :ordered_by_created_at, -> do
+    order :created_at => :desc
+  end
+
+end
