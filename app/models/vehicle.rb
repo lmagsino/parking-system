1bac6fe6abe707505011d5470dd7e22b4d19a4f3
Leo Magsino <leo.magsino819@gmail.com>
2024-02-25 09:34:04 +0800

    added parking transaction to model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -34,4 +34,20 @@ class Vehicle
     type == :large
   end
 
+
+
+  scope :plate_number_is, -> (plate_number) do
+    where 'lower(plate_number) = ?', plate_number.downcase
+  end
+
+
+
+  def latest_parking_transaction
+    self.parking_transactions.order(:start_time).last
+  end
+
+  def latest_completed_parking_transaction
+    self.parking_transactions.order(:start_time).completed.last
+  end
+
 end
